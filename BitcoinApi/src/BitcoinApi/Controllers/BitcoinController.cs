2a4cccacdb4b32918faf7d88c3c2055c4c061045
kevin <kevin.wang@ladbrokes.com.au>
2020-07-09 19:49:10 +1000

    create coinbase api with serverless

---
+++ BitcoinApi/src/BitcoinApi/Controllers/BitcoinController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using BitcoinApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BitcoinApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class BitcoinController : ControllerBase
+    {
+        private readonly ICoinbaseService _coinbaseService;
+
+        public BitcoinController(ICoinbaseService coinbaseService)
+        {
+            _coinbaseService = coinbaseService;
+        }
+
+        [HttpGet("{baseCurrency}")]
+        public async Task<IActionResult> Get(string baseCurrency)
+        {
+            try
+            {
+                var price = await _coinbaseService.GetBitcoinPrice("BTC", baseCurrency.ToUpper());
+
+                return Ok(price);
+            }
+            catch(Exception ex)
+            {
+                LambdaLogger.Log(ex.ToString());
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ BitcoinApi/src/BitcoinApi/Services/CoinbaseService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Coinbase;
+
+namespace BitcoinApi.Services
+{
+    public interface ICoinbaseService
+    {
+        Task<decimal> GetBitcoinPrice(string code, string baseCurrency);
+    }
+
+    public class CoinbaseService : ICoinbaseService
+    {
+        private readonly CoinbaseClient _coinbaseClient;
+        public CoinbaseService()
+        {
+            _coinbaseClient = new CoinbaseClient();
+        }
+
+        public async Task<decimal> GetBitcoinPrice(string code, string baseCurrency)
+        {
+            var r = await _coinbaseClient.Data.GetExchangeRatesAsync(code);
+
+            if (!r.Data.Currency.Equals(code, StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new Exception("Not found currency");
+            }
+
+            if(!r.Data.Rates.ContainsKey(baseCurrency))
+            {
+                throw new Exception("Not found base currency");
+            }
+
+            return r.Data.Rates[baseCurrency];
+        }
+    }
+}
